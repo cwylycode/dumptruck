fe37b33d152fe4ae4f9faa2f57156a98e9eeb9ee
hexadeci-male <joeisnomoe@mail.com>
2021-05-03 20:30:02 -0600

    Restructured repo

--- custom_enumerate.py
+++
@@ -1,4 +0,0 @@
-#Super short enumerate example (2-3x slower than the built-in enumerate function)
-my_list = ["chess","poodle","boo","hello"]
-def enumer(l):return zip([i for i in range(len(l))],l)
-for a,b in enumer(my_list):print(f"index: {a}, item: {b}")

5ac875353aee35de6afb1aa043af3b15f6f39285
hexadeci-male <joeisnomoe@mail.com>
2022-01-18 16:44:11 -0700

    add docs

--- python/musical_timing_recorder.py
+++ python/musical_timing_recorder.py
@@ -1,3 +1,13 @@
+"""
+This script is used for 'writing' songs in musical notation form, with recording of key downs and ups being used to define the time durations and delays of notes. Notes are shown line by line and a single key on your keyboard can be used to set the timing for each note in a song - of course, you'll need to know the song by heart.
+
+All that is needed is the pynput module installed and a file containing your music notes. You will need to pass the music notes file as an argument. To start, call this script from python in the terminal - e.g.:
+
+$ python3 musical_timing_recorder.py <path to your music notes file>
+
+Once all the music notes have been 'played' by you, an out file gets written to in the same directory as your music notes file. This text file will contain 3 separate sections - the notes, the note durations, and the note gaps.
+"""
+
 from pynput import keyboard
 import time
 import os,sys
@@ -20,13 +30,15 @@ def key_up(key):
 kl = keyboard.Listener(on_press=key_down,on_release=key_up,suppress=True)
 kl.start()
 
+# Execute
 if __name__ == "__main__":
     # Init
     if not DEBUG:
         args = sys.argv
         if os.path.isfile(args[1]): notes_file = args[1]
         else: raise FileNotFoundError("Error: missing positional argument - music notes file")
-    else: notes_file = "<INSERT MUSIC NOTES FILE HERE>"
+    else:
+        notes_file = "<INSERT MUSIC NOTES FILE HERE>" # Ignore this
     with open(notes_file,"r") as f:
         x = f.readlines()
         lines = [i.strip() for i in x]

23a56bc63cb43f7c93ed97a4d31da7991549c403
cwylycode <joeisnomoe@mail.com>
2022-12-25 15:01:50 -0700

    Initial set up of web stuff for gh-pages

--- cpp/asciibox.h
+++
@@ -1,4 +0,0 @@
-#include <iostream>
-using namespace std;
-
-void ascii_box_play(int loops, int size_min, int size_max, float speed);
--- cpp/pythagorus.h
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-
-void pythagorean_theorum(char solve = 'c');
